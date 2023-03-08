d35de340071c22c8fc261de5472f8972707c6d1e
Uladzislau_Komar <vladkomar25@gmail.com>
2023-03-08 18:13:24 +0300

    Adjusted the first screen

--- Sense/Scenes/Registration/RegistrationViewController.swift
+++ Sense/Scenes/Registration/RegistrationViewController.swift
@@ -15,7 +15,6 @@ class RegistrationViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
